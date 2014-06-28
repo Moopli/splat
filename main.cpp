e10222e6732a3d1ee3f4984ea168961711381f0f
e23walls <emily_walls23@hotmail.com>
2014-06-28 12:43:31 -0800

    Solved issue for storing opcodes correctly.

--- main.cpp
+++ main.cpp
@@ -5,32 +5,43 @@
 
 using namespace std;
 
-// TODO: Figure out how to best store the opcodes from the ROM
-//	 within some data structure (stringstream, string, array,
-//	 something else, etc.).
+// TODO: Don't hardcode the ROM path!
 
 int main(int argc, char ** argv)
 {
    ifstream rom("../c8games/PONG", fstream::in);
 
    stringstream opcodes;
-   // opcodes[i] requires being cast to an unsigned
-   // char and then an int (not unsigned int!) being useable.
-   // string opcodes = "";
+   // Each opcode is stored in four successive chars, followed by
+   // a newline. E.g., "5F20\n"
+   // No casting is necessary to access these, just use array
+   // syntax. To deal with the issue of the fact that we're
+   // taking the values from an int, if a value is less than
+   // F (i.e., 15), the opcode will be shorter. This is why
+   // there is a placeholder "0" appended to the string if
+   // the current int is too small.
    unsigned char a = 0;
    int count = 0;
-   while (rom >> hex >> a) // When dealing with hex stuff, we need this intermediate hex stream object
+   //  When dealing with hex stuff, we need this intermediate hex stream object
+   // Syntax:
+   // sstreamObject << hex << (int)currentVar;
+   while (rom >> hex >> a)
    {
-      cout << hex << (int)a << endl;
+      if (a < 16)
+          opcodes << "0"; // Placeholder for parsing
       opcodes << hex << (int)a;
       if (count % 2 == 1)
           opcodes << hex << endl;
       count++;
-      
-      // opcodes.push_back(a);
    }
    rom.close();
-   cout << "String:\n" << opcodes << endl;
+   // See what the data looks like yourself:
+   cout << "String:\n" << opcodes.str() << endl;
+   cout << "Or in loop:\n";
+   for (int i = 0; i < (int)opcodes.str().size(); i++)
+   {
+      cout << opcodes.str()[i];
+   }
    
    // stuff
    return 0;
