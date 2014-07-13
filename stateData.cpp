f200c6e31c475ca3fed84f7593b4b5e0514de96a
chrisdevisser <chris.n.devisser@gmail.com>
2014-07-13 17:58:54 -0400

    Simplified initialization of display
    Value-initialization will zero all the bools. Nested loop gone.

--- stateData.cpp
+++ stateData.cpp
@@ -2,15 +2,7 @@
 #include <CImg.h>
 using namespace cimg_library;
 
-CHIP8state::CHIP8state(string fn) : I(0), PC(512), delayTimer(0), soundTimer(0), filename(fn)
-{
+CHIP8state::CHIP8state(string fn)
+    : I(0), PC(512), delayTimer(0), soundTimer(0), display{}, filename(fn) {
    // this->cDisplay = new CImgDisplay(64, 32, "", 3, false, false);
-   // Make the display start out blank when ROM is loaded.
-   for (int j = 0; j < 32; j++)
-   {
-      for (int i = 0; i < 64; i++)
-      {
-         this->display[i][j] = false;
-      }
-   }
 }
